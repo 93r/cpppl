f35a129f576aa7753fb0f95712e9e66503edfe48
93r <ma_ko@riseup.net>
2018-11-23 14:17:05 +0100

    improve ex03.cpp

--- ch06/ex03.cpp
+++ ch06/ex03.cpp
@@ -6,6 +6,24 @@
 #include <numeric>
 #include <algorithm>
 
+template<typename T>
+T median(const std::vector<T>& v)
+{
+	T mdn {0};
+
+	if (v.size() % 2 == 0) {
+		mdn = (
+				*( v.begin() + v.size()/2 )
+				+ *( v.begin() + v.size()/2 - 1)
+		      )/2;
+	}
+	else {
+		mdn = *( v.begin() + v.size()/2 );
+	}
+
+	return mdn;
+}
+
 int main()
 {
 	std::ifstream in {"ex03.dat"};
@@ -14,58 +32,40 @@ int main()
 	std::map<std::string, std::vector<decltype(value)>> m;
 
 	while (in >> name >> value) {
-		//std::cout << name << " -> " << value << "\n";
 		m[name].push_back(value);
 	}
 
-	std::vector<double> totals;
+	std::vector<decltype(value)> totals;
 	for (auto& [k, v] : m) {
-		double sum = std::accumulate(v.begin(), v.end(), 0.0);
-		double avg = sum/v.size();
-		double median = 0.0;
+		decltype(value) sum = std::accumulate(v.begin(), v.end(), 0.0);
+		decltype(value) avg = sum/v.size();
+		decltype(value) mdn = 0.0;
 
 		std::sort(v.begin(), v.end());
-		if (v.size() % 2 == 0) {
-			median = (
-					*( v.begin() + v.size()/2 )
-					+ *( v.begin() + v.size()/2 - 1)
-				 )/2;
-		}
-		else {
-			median = *( v.begin() + v.size()/2 );
-		}
-
+		mdn = median(v);
 		totals.insert(totals.end(), v.begin(), v.end());
 
 		std::cout
 			<< k
 			<< " sum: " << sum
 			<< " avg: " << avg
-			<< " median: " << median
+			<< " median: " << mdn
 			<< "\n";
 	}
 
 	auto& v = totals;
-	double sum = std::accumulate(v.begin(), v.end(), 0.0);
-	double avg = sum/v.size();
-	double median = 0.0;
+	decltype(value) sum = std::accumulate(v.begin(), v.end(), 0.0);
+	decltype(value) avg = sum/v.size();
+	decltype(value) mdn = 0.0;
 
 	std::sort(v.begin(), v.end());
-	if (v.size() % 2 == 0) {
-		median = (
-				*( v.begin() + v.size()/2 )
-				+ *( v.begin() + v.size()/2 - 1)
-			 )/2;
-	}
-	else {
-		median = *( v.begin() + v.size()/2 );
-	}
+	mdn = median(v);
 
 	std::cout
 		<< "\n---TOTALS: "
 		<< " sum: " << sum
 		<< " avg: " << avg
-		<< " median: " << median
+		<< " median: " << mdn
 		<< "\n";
 }
 
